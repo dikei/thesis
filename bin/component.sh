368cfeab615a7da40078f3f849ca0b6f4297d52e
Kien Truong <duckientruong@gmail.com>
2016-03-31 15:47:49 +0100

    Update connected components

--- bin/component.sh
+++ bin/component.sh
@@ -3,4 +3,5 @@
 spark-submit --class pt.tecnico.spark.graph.ConnectedComponent \
     target/scala-2.10/thesis-assembly-1.0.jar \
     /home/dikei/Tools/tmp/spark-testing/data/web-Google.txt \
-    /home/dikei/Tools/tmp/spark-testing/out/connectedcomponent
+    /home/dikei/Tools/tmp/spark-testing/out/connectedcomponent \
+    8
--- src/main/scala/pt/tecnico/spark/graph/ConnectedComponent.scala
+++ src/main/scala/pt/tecnico/spark/graph/ConnectedComponent.scala
@@ -11,15 +11,16 @@ object ConnectedComponent {
 
   def main(args: Array[String]): Unit = {
 
-    if (args.length < 3) {
+    if (args.length < 2) {
       println("Usage: ")
-      println("spark-submit --class pt.tecnico.spark.graph.ConnectedComponent [jar] [input] [output] [statDir]")
+      println("spark-submit --class pt.tecnico.spark.graph.ConnectedComponent [jar] [input] [output] [#numOfPartitions] [statDir]")
       System.exit(0)
     }
 
     val input = args(0)
     val output = args(1)
-    val statsDir = if (args.length > 2) args(2) else "stats"
+    val partitionCount = if (args.length > 2) args(2).toInt else -1
+    val statsDir = if (args.length > 3) args(3) else "stats"
     val conf = new SparkConf().setAppName("ConnectedComponent")
     conf.set("spark.hadoop.validateOutputSpecs", "false")
     val sc = new SparkContext(conf)
@@ -27,7 +28,7 @@ object ConnectedComponent {
     val listener = new StageRuntimeReportListener(statsDir)
     sc.addSparkListener(listener)
 
-    val graph = GraphLoader.edgeListFile(sc, input)
+    val graph = GraphLoader.edgeListFile(sc, input, numEdgePartitions = partitionCount)
 
     // Calculate and save the connected components
     graph.connectedComponents().vertices.saveAsTextFile(output)
