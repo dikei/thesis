9dcb4a4937b2583d029123159150948ced9ada96
Kien Truong <duckientruong@gmail.com>
2016-06-15 15:14:32 +0100

    Update script

--- bin/freqwordcount-nobr.sh
+++ bin/freqwordcount-nobr.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-spark-submit --class pt.tecnico.spark.WordCount \
+spark-submit --conf "spark.scheduler.removeStageBarrier=true" \
+    --class pt.tecnico.spark.FrequentWordCount \
     target/scala-2.10/thesis-assembly-1.0.jar \
     /home/dikei/Tools/tmp/spark-testing/data/pride.txt \
-    300
+    300 \
+    4
--- bin/freqwordcount.sh
+++ bin/freqwordcount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-spark-submit --class pt.tecnico.spark.WordCount \
+spark-submit --class pt.tecnico.spark.FrequentWordCount \
     target/scala-2.10/thesis-assembly-1.0.jar \
     /home/dikei/Tools/tmp/spark-testing/data/pride.txt \
-    /home/dikei/Tools/tmp/spark-testing/out/pride \
-    /home/dikei/Programming/Scala/spark/thesis/stats
+    300 \
+    4
