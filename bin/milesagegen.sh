3801287be5aa74859744a8fb4e8077322d3742ff
Kien Truong <duckientruong@gmail.com>
2016-06-22 15:00:44 +0100

    Update Milesagegen script

--- bin/milesagegen.sh
+++ bin/milesagegen.sh
@@ -2,9 +2,9 @@
 
 spark-submit --class pt.tecnico.spark.milesage.MilesageGen \
     target/scala-2.10/thesis-assembly-1.0.jar \
-    10000000 \
+    2500000 \
     100000 \
-    20 \
+    10 \
     8 \
     /home/dikei/Tools/tmp/spark-testing/out/passenger \
     /home/dikei/Tools/tmp/spark-testing/out/flight
