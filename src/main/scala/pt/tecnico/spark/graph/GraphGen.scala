94be3ec8affedb38d91ae62a2dc51609cfd46d1a
Kien Truong <duckientruong@gmail.com>
2016-07-21 13:06:07 +0100

    Don't partition graph when generating
    Partition use a lot of memory, we won't be able to generate very large
    graphs

--- src/main/scala/pt/tecnico/spark/graph/GraphGen.scala
+++ src/main/scala/pt/tecnico/spark/graph/GraphGen.scala
@@ -1,6 +1,6 @@
 package pt.tecnico.spark.graph
 
-import org.apache.spark.graphx.{GraphXUtils, PartitionStrategy}
+import org.apache.spark.graphx.{GraphXUtils}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.graphx.util.GraphGenerators
 
@@ -24,13 +24,13 @@ object GraphGen {
     val conf = new SparkConf().setAppName("GraphGen")
     conf.set("spark.hadoop.validateOutputSpecs", "false")
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+
     GraphXUtils.registerKryoClasses(conf)
 
     val sc = new SparkContext(conf)
 
     val graph = GraphGenerators
       .logNormalGraph(sc, noVertices, seed = randomSeed, numEParts = noPartition)
-      .partitionBy(PartitionStrategy.EdgePartition2D)
 
     graph.edges.map { edge =>
       edge.srcId + " " + edge.dstId
