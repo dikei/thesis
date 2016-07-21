d7963863f34a32f0fbfddf6ae3b066e7edb9f7c4
Kien Truong <duckientruong@gmail.com>
2016-07-21 13:04:53 +0100

    Update distribution script

--- dist.sh
+++ dist.sh
@@ -7,5 +7,7 @@ cp -rf bin thesis/
 cp -rf target/scala-2.10/thesis-assembly-1.0.jar thesis/target/scala-2.10
 tar czf thesis.tar.gz thesis
 
+rm -rf thesis
+
 echo "Upload to gateway"
 scp thesis.tar.gz ktruong@cloudtm.ist.utl.pt:/home/ktruong/nas/cloudtm/thesis.tar.gz
